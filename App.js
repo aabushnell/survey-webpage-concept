6675ad93a56ab14fde962e815dc323a01bdc0063
Aaron Bushnell <aabushnell@gmail.com>
2023-03-07 20:33:42 +0100

    report final values

--- App.js
+++ App.js
@@ -9,11 +9,14 @@ for (var i = 0; i < buttons.length; i++) {
 
     var target = this.getAttribute('data-next');
     var dvalue = this.getAttribute('value-change');
+    var bid_value = parseInt(document.getElementById('bid').value);
 
     value += parseInt(dvalue);
     console.log(value);
     document.getElementById('value1').innerHTML = value;
     document.getElementById('value2').innerHTML = value;
+    document.getElementById('value3').innerHTML = value;
+    document.getElementById('bid_value').innerHTML = bid_value;
 
     for (var j = 0; j < panels.length; j++) {
       var panel = panels[j];
